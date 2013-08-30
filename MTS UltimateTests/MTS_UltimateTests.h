f0121d809c152c4a0fc431453a09b8f15dbb055d
Felipe Macbook Pro <FelipeOtalora@Felipe-Otaloras-MacBook.local>
2013-08-29 22:37:17 -0500

    Initial Commit

---
+++ MTS UltimateTests/MTS_UltimateTests.h
@@ -0,0 +1,13 @@
+//
+//  MTS_UltimateTests.h
+//  MTS UltimateTests
+//
+//  Created by Felipe Macbook Pro on 8/29/13.
+//  Copyright (c) 2013 Felipe Macbook Pro. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface MTS_UltimateTests : SenTestCase
+
+@end
---
+++ MTS UltimateTests/MTS_UltimateTests.m
@@ -0,0 +1,32 @@
+//
+//  MTS_UltimateTests.m
+//  MTS UltimateTests
+//
+//  Created by Felipe Macbook Pro on 8/29/13.
+//  Copyright (c) 2013 Felipe Macbook Pro. All rights reserved.
+//
+
+#import "MTS_UltimateTests.h"
+
+@implementation MTS_UltimateTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in MTS UltimateTests");
+}
+
+@end
